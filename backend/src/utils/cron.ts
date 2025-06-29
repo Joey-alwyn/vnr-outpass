224e269878addeb64bad0b3fd9de2c5e4d4c913b
Madhav-Sarma <a.madhavsarma@gmail.com>
2025-06-29 18:19:53 +0530

    cron-2

--- backend/src/utils/cron.ts
+++ backend/src/utils/cron.ts
@@ -1,17 +1,19 @@
-import cron from 'cron';
-import https from 'https';
-import { API_URL } from '../config';
+import { CronJob } from "cron";
+import https from "https";
+import { API_URL } from "../config";
 
-const job = new cron.CronJob('*/14 * * * *', function () {
+const job = new CronJob("*/14 * * * *", function () {
   if (!API_URL) {
-    console.error('API_URL is not defined.');
+    console.error("API_URL is not defined.");
     return;
   }
 
-  https.get(API_URL, (res) => {
-    if (res.statusCode === 200) console.log('GET request sent successfully');
-    else console.log('GET request failed', res.statusCode);
-  }).on('error', (e) => console.error('Error while sending request', e));
+  https
+    .get(API_URL, (res) => {
+      if (res.statusCode === 200) console.log("GET request sent successfully");
+      else console.log("GET request failed", res.statusCode);
+    })
+    .on("error", (e) => console.error("Error while sending request", e));
 });
 
 export default job;
